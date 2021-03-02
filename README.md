052405b683abb2e3a4999161516d4d1dff30dddd
Mika Yeap <yeapmika@gmail.com>
2021-03-02 17:36:26 +0800

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# RELM - Real Estate Listings Manager
+
+Website to manage property listings. 
+
+## Local Dev
+
+`npm run dev`
+
+NOTE: `npm  start` + `npm run dev` scripts in `package.json` modified to use host 0.0.0.0 and port 62103.
+
+## Docker
+
+`docker build -t app-img .`
+
+`docker run --name app-name -p 62103:62103 app-img`
+
+Using LTS Node Alpine image for final stage of build.
---
+++ src/App.svelte
@@ -0,0 +1,82 @@
+<script>
+	import Navbar from "./Navbar.svelte";
+
+	//set when user logs in
+	let user = {
+		id: "",
+	};
+	let userLogin = {
+		email: "",
+		password: "",
+	};
+
+	$: isLoggedIn = user.id !== "" && user.id !== undefined;
+</script>
+
+<main>
+	<Navbar {...user} />
+	<div class="container">
+		{#if !isLoggedIn}
+			<div class="row">
+				<div class="col-sm col-md-8" id="info-col">
+					<h1 class="brand">RELM</h1>
+					<p>
+						<strong>R</strong>eal <strong>E</strong>state
+						<strong>L</strong>isting <strong>M</strong>anagement.
+					</p>
+					<p>
+						Designed by <a href="https://myika.co"
+							>Myika Technologies</a
+						>.
+					</p>
+				</div>
+				<div class="col-sm col-md-4" id="login-col">
+					<h4 class="section-head">Log In</h4>
+					<div class="form">
+						<div class="mb-3">
+							<label for="email" class="form-label"> Email</label>
+							<input
+								type="email"
+								class="form-control"
+								id="email"
+								placeholder="name@agency.com"
+								bind:value={userLogin.email}
+							/>
+						</div>
+						<div class="mb-3">
+							<label for="password" class="form-label">
+								Password</label
+							>
+							<input
+								type="password"
+								class="form-control"
+								id="password"
+								placeholder="password"
+								bind:value={userLogin.password}
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		{:else}
+			<h1>Logged in!</h1>
+		{/if}
+	</div>
+</main>
+
+<style type="text/scss">
+	@import "./styles/_all";
+
+	#info-col {
+		text-align: center;
+		padding: 3rem;
+		font-size: large;
+	}
+	strong {
+		color: $blue;
+	}
+	#login-col {
+		text-align: left;
+		padding: 4rem auto;
+	}
+</style>
