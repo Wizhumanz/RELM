67bc931b4e4cad51c76a898f1c51fbad04da13ff
Mika Yeap <yeapmika@gmail.com>
2021-03-12 14:01:07 +0800

    basic POST listing route

--- api/api.go
+++ api/api.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"cloud.google.com/go/datastore"
 )
@@ -26,84 +27,111 @@ type User struct {
 	Email string
 }
 
+type Listing struct {
+	Name        string `json:"name"` // immutable once created, used for queries
+	Address     string `json:"address"`
+	Postcode    string `json:"postcode"`
+	Price       int    `json:"price"`
+	ListingType int    `json:"type"` // 0 = for rent, 1 = for sale
+	ImgURL      string `json:"img"`
+}
+
+func (l Listing) String() string {
+	return l.Name + " / " + l.Address + " / " + l.Postcode
+}
+
+type newListingPostReq struct {
+	Auth        string      `json:"auth"`
+	Name        string      `json:"name"`
+	Address     string      `json:"address"`
+	Postcode    string      `json:"postcode"`
+	Price       json.Number `json:"price"`
+	ListingType json.Number `json:"type"` // 0 = for rent, 1 = for sale
+	ImgURL      string      `json:"img"`
+}
+
+var googleProjectID = "myika-relm"
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	// var data jsonResponse
-	// if r.Method != "GET" {
-	// 	data = jsonResponse{Msg: "Only GET Allowed", Body: "This endpoint only accepts GET requests."}
-	// 	w.WriteHeader(http.StatusUnauthorized)
-	// } else {
-	// 	data = jsonResponse{Msg: "привет сука", Body: "курица - дерьмо"}
-	// 	w.WriteHeader(http.StatusOK)
-	// }
+	var data jsonResponse
+	if r.Method != "GET" {
+		data = jsonResponse{Msg: "Only GET Allowed", Body: "This endpoint only accepts GET requests."}
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	data = jsonResponse{Msg: "RELM API", Body: "Ready"}
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(data)
+	// w.Write([]byte(`{"msg": "привет сука"}`))
+}
 
-	// w.Header().Set("Content-Type", "application/json")
-	// json.NewEncoder(w).Encode(data)
-	// // w.Write([]byte(`{"msg": "привет сука"}`))
+func createNewListing(w http.ResponseWriter, r *http.Request) {
+	var data jsonResponse
+	var newListingReq newListingPostReq
 
+	if r.Method != "POST" {
+		data = jsonResponse{Msg: "Only POST Allowed", Body: "This endpoint only accepts POST requests."}
+		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode(data)
+		return
+	}
+
+	// decode data
+	err := json.NewDecoder(r.Body).Decode(&newListingReq)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// TODO: check real auth
+	auth := os.Getenv("AUTH")
+	if auth != newListingReq.Auth {
+		data = jsonResponse{Msg: "Authorization Invalid", Body: "Auth field value from body invalid."}
+		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode(data)
+		return
+	}
+
+	// create new listing in DB
 	ctx := context.Background()
-
-	// Set your Google Cloud Platform project ID.
-	projectID := "myika-relm"
-
-	// Creates a client.
-	client, err := datastore.NewClient(ctx, projectID)
+	client, err := datastore.NewClient(ctx, googleProjectID)
 	if err != nil {
 		log.Fatalf("Failed to create client: %v", err)
 	}
 
-	// Sets the kind for the new entity.
-	kind := "User"
-	// Sets the name/ID for the new entity.
-	name := "user0"
-	// Creates a Key instance.
-	newUserKey := datastore.NameKey(kind, name, nil)
-
-	// Creates a Task instance.
-	newUser := User{
-		Name:  "Mika Yeap",
-		Email: "mika@myika.co",
+	kind := "Listing"
+	name := time.Now().Local().String() //id
+	newListingKey := datastore.NameKey(kind, name, nil)
+	price, _ := newListingReq.Price.Int64()
+	lType, _ := newListingReq.ListingType.Int64()
+	newListing := Listing{
+		Name:        newListingReq.Name,
+		Address:     newListingReq.Address,
+		Postcode:    newListingReq.Postcode,
+		Price:       int(price),
+		ListingType: int(lType),
+		ImgURL:      newListingReq.ImgURL,
 	}
 
-	// Saves the new entity.
-	if _, err := client.Put(ctx, newUserKey, &newUser); err != nil {
-		log.Fatalf("Failed to save User: %v", err)
+	if _, err := client.Put(ctx, newListingKey, &newListing); err != nil {
+		log.Fatalf("Failed to save Listing: %v", err)
 	}
 
-	fmt.Printf("Saved %v: %v\n", newUserKey, newUser.Name+" - "+newUser.Email)
-}
-
-func createNewArticle(w http.ResponseWriter, r *http.Request) {
-	var data jsonResponse
-	if r.Method != "POST" {
-		data = jsonResponse{Msg: "Only POST Allowed", Body: "This endpoint only accepts POST requests."}
-		w.WriteHeader(http.StatusUnauthorized)
-	} else {
-		// decode data
-		var emailReq jsonEmailPostRequest
-		err := json.NewDecoder(r.Body).Decode(&emailReq)
-		if err != nil || emailReq.Auth == "" || emailReq.Email == "" {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		// check emailReq.Auth with env var
-		auth := os.Getenv("AUTH")
-		if auth != emailReq.Auth {
-			data = jsonResponse{Msg: "Authorization Invalid", Body: "Auth field value from body invalid."}
-			w.WriteHeader(http.StatusUnauthorized)
-		} else {
-			//TODO: do something
-		}
+	// return
+	data = jsonResponse{
+		Msg:  "Saved " + newListingKey.String(),
+		Body: newListing.String(),
 	}
-
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(data)
 }
 
 func main() {
 	muxRouter := http.NewServeMux()
 	muxRouter.HandleFunc("/", indexHandler)
-	muxRouter.HandleFunc("/email", createNewArticle)
+	muxRouter.HandleFunc("/listing", createNewListing)
 
 	auth := os.Getenv("AUTH")
 	fmt.Println("AUTH var = " + auth)
