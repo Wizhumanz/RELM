5bcb96a6f6c6e7d0bb9705f95b161a60e71efb6b
Mika Yeap <yeapmika@gmail.com>
2021-03-14 11:06:09 +0800

    verified working w/ Sass and sapper index comp

--- frontend/src/__tests__/index.js
+++ frontend/src/__tests__/index.js
@@ -1,8 +1,9 @@
 import '@testing-library/jest-dom/extend-expect'
 
-import { render, fireEvent } from '@testing-library/svelte'
+import { render } from '@testing-library/svelte'
 
-import { Index } from '../routes/index'
+//TODO: ParseError when Sass styles in component
+import Index from '../routes/index.svelte'
 
 test('shows proper heading when rendered', () => {
   const { getByText } = render(Index)
