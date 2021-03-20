d867f9c60216a3837b1aa801f1a2f85874c59ddf
Simon Jeong <simon20000808@gmail.com>
2021-03-20 13:24:23 +0900

    Added input for the features

--- frontend/src/routes/add.svelte
+++ frontend/src/routes/add.svelte
@@ -1,10 +1,46 @@
 <script>
   import axios from "axios";
+  import { onMount } from 'svelte';
+  import { storeUser } from "../../store.js";
+  
+  let user = {};
+  storeUser.subscribe((newValue) => {
+    if (newValue) {
+      user = JSON.parse(newValue);
+    }
+  });
+	
+	let now = new Date(), month, day, year;
+  let files
+  let filesStr = []
+  let owner = ''
+  let name = ''
+  let address = ''
+  let postcode = ''
+  let area = ''
+  let price = 0
+  let rentBuyOption
+  let propertyType
+  let listingType
+  let dateString
+  let isPublic = false
+  let isCompleted = false
+  let isPending = false
 
-  let files;
-  let filesStr = [];
+  onMount(() => {
+        month = '' + (now.getMonth() +1),
+        day = '' + now.getDate(),
+        year = now.getFullYear();
 
-  function uploadImgs() {
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    dateString = [year, month, day].join('-');
+	})
+
+  function uploadImgs(e) {
     files = document.querySelector("[type=file]").files;
     for (let i = 0; i < files.length; i++) {
       let file = files[i];
@@ -24,14 +60,13 @@
 
       // document.getElementById("imgDisplay").innerHTML = newImage.outerHTML;
       // console.log("Base64 img = " + newImage.src);
-      filesStr.push(newImage.src);
+      filesStr.push(newImage.src)
     };
     fileReader.readAsDataURL(fi);
   }
 
   function addListing() {
-    //convert uploaded images to base64 encoded strings
-    uploadImgs();
+    console.log("adding listing");
 
     const hds = {
       "Cache-Control": "no-cache",
@@ -39,27 +74,28 @@
       Expires: "0",
       auth: "agent",
     };
-
+    //Don't change any of these properties
     let data = {
-      name: "TEST IMG",
-      imgs: filesStr,
-      user: "agent@agent.com",
-      owner: "owner@owner.com",
-      address: "Jalan Besar 7",
-      postcode: "10130",
-      area: "Teluk Bahang",
-      price: "8000",
-      propertyType: "1",
-      listingType: "1",
-      availableDate: "",
-      isPublic: "true",
-      isCompleted: "false",
-      isPending: "false",
+      name: name, //name of listings are immutable
+      imgs: filesStr, 
+      user: user.id, //get user.id from store.js
+      owner: owner,
+      address: address,
+      postcode: postcode,
+      area: area,
+      price: price,
+      propertyType: propertyType,
+      listingType: listingType,
+      availableDate: dateString,
+      isPublic: isPublic,
+      isCompleted: isCompleted,
+      isPending: isPending,
     };
+    console.log(data)
 
     axios
       .post("http://localhost:8000/listing", data, {
-        headers: hds,
+        headers: hds
       })
       .then((res) => {
         console.log(res.status + " -- " + JSON.stringify(res.data));
@@ -69,22 +105,37 @@
 </script>
 
 <div class="container">
-  <h1 id="head">Add Listing</h1>
+  <h1>Add Listing</h1>
 
   <div class="row">
     <div class="col-sm col-md-3">
       <button id="excel-upload">Upload Excel</button>
     </div>
-
     <div class="col-sm col-md-9">
-      <div id="manual-add-box">
-        <!-- <div id="imgDisplay"></div> -->
-        <form method="post" enctype="multipart/form-data">
-          <label for="imgUploadInput">Upload images</label>
-          <input id="imgUploadInput" type="file" name="files[]" placeholder="Upload images" multiple />
-        </form>
+      <!-- file upload -->
+      <!-- <div id="imgDisplay"></div> -->
+      <form
+        method="post"
+        enctype="multipart/form-data"
+        on:submit|preventDefault={uploadImgs}
+      >
+        <input type="file" name="files[]" multiple />
+        <input type="submit" value="Upload File" name="submit" />
+      </form>
 
+      <div id="manual-add-box">
+        <h4 class="section-head">Manual Add</h4>
         <form class="form" on:submit|preventDefault={addListing}>
+          <div class="mb-3">
+            <label for="owner" class="form-label">Owner</label>
+            <input
+              type="text"
+              class="form-control"
+              id="owner"
+              placeholder="owner@owner.com"
+              bind:value={owner}
+            />
+          </div>
           <div class="mb-3">
             <label for="name" class="form-label">Name</label>
             <input
@@ -92,6 +143,7 @@
               class="form-control"
               id="name"
               placeholder="Premium Condo"
+              bind:value={name}
             />
           </div>
           <div class="mb-3">
@@ -101,14 +153,39 @@
               class="form-control"
               id="address"
               placeholder="38-A Skyhome, Jalan Tanjung Tokong"
+              bind:value={address}
             />
           </div>
           <div class="mb-3">
-            <label for="listingType" class="form-label">Address</label>
-            <select id="listingType" class="form-select">
+            <label for="address" class="form-label">Area</label>
+            <input
+              type="text"
+              class="form-control"
+              id="address"
+              placeholder="Teluk Bahang"
+              bind:value={area}
+            />
+          </div>
+          <div class="mb-3">
+            <label for="address" class="form-label">Postcode</label>
+            <input
+              type="text"
+              class="form-control"
+              id="address"
+              placeholder="10130"
+              bind:value={postcode}
+            />
+          </div>
+          <div class="mb-3">
+            <label for="rentBuy" class="form-label">Rent/Buy</label>
+            <select id="rentBuy" class="form-select" bind:value={rentBuyOption}>
               <option value="Rent">Rent</option>
               <option value="Buy">Buy</option>
             </select>
+          </div>
+          <div class="mb-3">
+            <label for="availableDate" class="form-label">Available Date</label>
+            <input id="availableDate" type=date bind:value={dateString}>
           </div>
           <div class="mb-3">
             <label for="price" class="form-label">Price</label>
@@ -117,7 +194,36 @@
               class="form-control"
               id="price"
               placeholder="RM 1,000"
+              bind:value={price}
             />
+          </div>
+          <div class="mb-3">
+            <label for="listingType" class="form-label">Listing Type</label>
+            <select id="listingType" class="form-select" bind:value={listingType}>
+              <option value="0">For Rent</option>
+              <option value="1">For Sale</option>
+            </select>
+          </div>
+          <div class="mb-3">
+            <label for="propertyType" class="form-label">Property Type</label>
+            <select id="propertyType" class="form-select" bind:value={propertyType}>
+              <option value="0">Landed</option>
+              <option value="1">Apartment</option>
+            </select>
+          </div>
+          <div class="mb-3">
+            <label>
+              <input type=checkbox bind:checked={isPublic}>
+              Public
+            </label>
+            <label>
+              <input type=checkbox bind:checked={isCompleted}>
+              Completed
+            </label>
+            <label>
+              <input type=checkbox bind:checked={isPending}>
+              Pending
+            </label>
           </div>
           <button type="submit">Add</button>
         </form>
@@ -134,18 +240,10 @@
     padding-bottom: 4rem;
   }
 
-  #head {
-    margin-bottom: 2.5rem;
-  }
-
   #manual-add-box {
     padding: 0 4rem;
     text-align: left;
     border-left: 1px solid $blue;
-  }
-
-  label {
-    margin-bottom: 0.5rem;
   }
 
   input,
