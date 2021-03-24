a50e76b0a237b52f495f785ed368ccf747f80961
Mika Yeap <yeapmika@gmail.com>
2021-03-25 02:13:09 +0800

    basic testing setup

--- api/api_test.go
+++ api/api_test.go
@@ -1,10 +1,35 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
+	"os"
 	"testing"
 )
 
-func TestMain(t *testing.T) {
-	fmt.Println("Tests working!")
+type testPair struct {
+	Input    interface{}
+	Expected interface{}
+}
+
+var testData []testPair
+
+func setup() {
+	testData := testPair{
+		testPair{Input: []int{1, 5, 6, 3, 9}, Expected: []int{1, 5, 6, 3}},
+	}
+}
+
+func shutdown() {
+
+}
+
+func TestMain(m *testing.M) {
+	setup()
+	code := m.Run()
+	shutdown()
+	os.Exit(code)
+}
+
+func TestDeleteElement(t *testing.T) {
+
 }
