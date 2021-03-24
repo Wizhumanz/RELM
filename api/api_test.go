1b31fecbb275af04b8030a5a30e20c18c6a46076
Mika Yeap <yeapmika@gmail.com>
2021-03-25 03:19:07 +0800

    basic test helper func

--- api/api_test.go
+++ api/api_test.go
@@ -3,24 +3,52 @@ package main
 import (
 	// "fmt"
 	"os"
+	"reflect"
 	"testing"
 )
 
 type testPair struct {
-	Input    interface{}
+	Inputs   []interface{}
 	Expected interface{}
 }
 
 var testData []testPair
 
 func setup() {
-	testData := testPair{
-		testPair{Input: []int{1, 5, 6, 3, 9}, Expected: []int{1, 5, 6, 3}},
+	testData = []testPair{
+		{
+			Inputs: []interface{}{
+				[]Listing{
+					{KEY: "A"},
+					{KEY: "B"},
+					{KEY: "C"},
+				},
+				Listing{KEY: "C"},
+			},
+			Expected: []Listing{
+				{KEY: "A"},
+				{KEY: "B"},
+			},
+		},
+		{
+			Inputs: []interface{}{
+				[]Listing{
+					{KEY: "D"},
+					{KEY: "E"},
+					{KEY: "F"},
+				},
+				Listing{KEY: "D"},
+			},
+			Expected: []Listing{
+				{KEY: "E"},
+				{KEY: "F"},
+			},
+		},
 	}
 }
 
 func shutdown() {
-
+	//shutdown
 }
 
 func TestMain(m *testing.M) {
@@ -31,5 +59,13 @@ func TestMain(m *testing.M) {
 }
 
 func TestDeleteElement(t *testing.T) {
-
+	for _, pair := range testData {
+		arg1 := pair.Inputs[0].([]Listing)
+		arg2 := pair.Inputs[1].(Listing)
+		expected := pair.Expected.([]Listing)
+		res := deleteElement(arg1, arg2)
+		if !reflect.DeepEqual(res, expected) {
+			t.Errorf("Expected deleteElement(%s, %s) to equal %s, actual result = %s", arg1, arg2, expected, res)
+		}
+	}
 }
